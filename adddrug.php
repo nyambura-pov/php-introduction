e665e6895ca8617c77e8d4bbe68915e47bd15b8a
nyambura-pov <faith.njuguna@strathmore.edu>
2023-05-17 09:38:49 +0300

    Creating the database

---
+++ adddrug.php
@@ -0,0 +1,9 @@
+<?php 
+require_once("connection.php");
+$drug_id = "1171";
+$drug_name = "Celestamine";
+
+$drug_id = "1182";
+$drug_name = "Metronidazole";
+
+?>
---
+++ addpatient.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once("connection.php");
+$first_name = "Mary";
+$email = "mary@gmail.com";
+$phone = "0723456789";
+$last_name = "Jones";
+
+$first_name = "Billy";
+$email = "billy@gmail.com";
+$phone = "0743345678";
+$last_name = "West";
+
+$first_name = "Grace";
+$email = "grace@gmail.com";
+$phone = "0711625678";
+$last_name = "Philly";
+
+?>
