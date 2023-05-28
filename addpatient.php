fb9708a992c45cc8043f43334c0525e1b6e226c9
nyambura-pov <faith.njuguna@strathmore.edu>
2023-05-28 23:40:07 +0300

    Adding records

--- addpatient.php
+++ addpatient.php
@@ -16,4 +16,9 @@ $email = "grace@gmail.com";
 $phone = "0711625678";
 $last_name = "Philly";
 
+$first_name = "Claire";
+$email = "claire@gmail.com";
+$phone = "0711725678";
+$last_name = "Wambui";
+
 ?>
--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "patientdb";
+$dbname = "ePharmadb";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
@@ -11,23 +11,4 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
-
-
-$sql = "INSERT INTO patients (first_name, email, phone, last_name)
-VALUES('Grace','grace@gmail.com','0711625678','Philly')";
-
-$sql = "INSERT INTO drugs (drug_id, drug_name)
-VALUES('1171','Celestamine')";
-
-echo($sql);
-
-if(mysqli_query($conn, $sql)){
-  echo "New record created successfully";
-} 
-else {
-  echo "Error: " .$sql . "<br>" . $conn->error;
-}
-
-$conn->close();
-
 ?>
